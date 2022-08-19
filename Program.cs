a14c3f122af742855f07f6f2bcc2d5b7538d349e
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-19 21:32:59 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace EmployeeWageProblem
+{
+    class program
+    {
+        public static void Main(string[] agrs)
+        {
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computerEmpWage();
+            Console.WriteLine("Total Wage for DMart Company : " + empWageBuilder.getTotalWage("DMart"));
+        }
+    }
+}
