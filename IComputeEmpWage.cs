fc1158dc2652c2953438ff6b60c9fc044c0f452d
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-19 21:35:12 +0530

    IComputeEmpWage.cs

---
+++ IComputeEmpWage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public interface IComputeEmpWage
+    {
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int MaxHoursPerMonth);
+        public void computeEmpWage();
+        public int getTotalWage(string company);
+    }
+}
